8cdd0dc8f10a0d46127f39003691e60948c3fbe5
Chayanika Basak <cb250802@gmail.com>
2023-06-14 13:09:39 +0530

    changes

--- pages/firebase.js
+++ pages/firebase.js
@@ -3,12 +3,12 @@ import {getAuth} from "firebase/auth";
 import {getFirestore, firestore} from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIPnXczIbVwudVZati8-OHRVSylBFVOTs",
-    authDomain: "hackunicorn-8a1ce.firebaseapp.com",
-    projectId: "hackunicorn-8a1ce",
-    storageBucket: "hackunicorn-8a1ce.appspot.com",
-    messagingSenderId: "758574644115",
-    appId: "1:758574644115:web:bd641b1e041c7567074666"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
 };
 
 
